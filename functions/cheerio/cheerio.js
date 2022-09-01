eca60d6a4b5200915e40787f4868e3c23ce2f0a1
Chris <chris@Chriss-Air.lan>
2022-08-31 20:19:14 -0700

    Initial build of site

---
+++ functions/cheerio/cheerio.js
@@ -0,0 +1,38 @@
+const request = require('axios')
+const cheerio = require('cheerio')
+
+exports.handler = async (event, context) => {
+  const url = 'https://www.multco.us/health/staying-healthy/wood-burning-restrictions'
+  try {
+    const { data } = await request(url)
+    const $ = cheerio.load(data)
+    /* queryDOM */
+    const updatedTime = $('meta[property="og:updated_time"]').attr('content');
+    const burnText = $("#node-37292 > div > div > p:nth-child(5)").text();
+    let burnBan = "";
+    const burnPhrase = "mandatory burn ban";
+
+    if (burnText.includes(burnPhrase)) {
+          burnBan = "true";
+    } else {
+          burnBan = "false";
+    }
+    console.log('burnText', burnText)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        updatedTime: updatedTime,
+        burnBan: burnBan,
+        burnText: burnText,
+        source: url
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error
+      })
+    }
+  }
+}
